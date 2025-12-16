9787b31ad1f35b9b6a3668dcabbfa3b5db61a1c6
mohamed mostafa <mohmost25@gmail.com>
2025-12-16 03:20:29 +0200

    add product and category models

---
+++ src/main/java/com/mohamedmostafa/IMS_project/models/Product.java
@@ -0,0 +1,60 @@
+package com.mohamedmostafa.IMS_project.models;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "products")
+@Data
+@Builder
+public class Product {
+    private final LocalDateTime createdAt = LocalDateTime.now();
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+
+    @Column(unique = true)
+    @NotBlank(message = "SKU is required")
+    private String sku;
+
+
+    @Positive(message = "product price must be a positive value")
+    private BigDecimal price;
+
+
+    @Min(value = 0, message = "stock quantity cannot be negative")
+    private Integer stockQuantity;
+
+
+    private String description;
+
+
+    private LocalDateTime expiryDate;
+
+
+    private String imageUrl;
+
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+}
